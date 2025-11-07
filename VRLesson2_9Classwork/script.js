7d05a6be4f21a08f3e9be8fda3a8d682133b3a56
lgao1011 <lucasg61@nycstudents.net>
2025-11-07 17:26:00 +0000

    update 2.9

--- VRLesson2_9Classwork/script.js
+++ VRLesson2_9Classwork/script.js
@@ -1,16 +1,16 @@
 let maze = [
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
+  "---------ToTTTTTTTxTxTTTTTTT-------------",
+  "--------------T---oxo-----",
+  "---------ooo------oo--o-x----",
+  "-----oooxoooooooooooxoxoooooooooo--oooooooooooo---------------",
+  "-------x-x---T-T--ox----o--o",
+  "----x--x-TT----oxx---xxxox----o----",
+  "-----x--x----xo---Tx-xo-------oooooo",
+  "------xxx-xxT--xo-x-xox-----o------",
+  "-TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT-------xxxxx--xoxxoxx--o---------TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT-",
+  "----------x-xxx-xxo---o---o---ooo-",
+  "--------xx-----x-o-T-------",
+  "----------xxxxT------------",
 ];
 let scene;
 
